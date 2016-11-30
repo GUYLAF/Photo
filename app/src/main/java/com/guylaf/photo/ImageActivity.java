1821bd574769a6b19ef72ef23a473d5c7591431b
GUY LAFORET <GUY LAFORET>
2016-11-30 09:14:13 +0100

    UPDATE Spinner

--- app/src/main/java/com/guylaf/photo/ImageActivity.java
+++ app/src/main/java/com/guylaf/photo/ImageActivity.java
@@ -4,19 +4,29 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.*;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.guylaf.photos.R;
 
 import java.util.ArrayList;
@@ -24,12 +34,22 @@ import java.util.List;
 
 public class ImageActivity extends AppCompatActivity implements InterfaceResponse {
 
+    private static SharedPreferences getPr;
     private BoundService boundService;
     boolean bound = false;
     private List<Photo> list = new ArrayList<>();
     private List<Photo> listOne = new ArrayList<>();
     private ImageAdapter imageAdapter;
     private DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mDrawerToggle;
+    private SharedPreferences prefs;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private com.google.android.gms.common.api.GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,100 +57,147 @@ public class ImageActivity extends AppCompatActivity implements InterfaceRespons
         setContentView(R.layout.activity);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(
+        mDrawerToggle = new ActionBarDrawerToggle(
                 this,                  /* host Activity */
                 mDrawerLayout,         /* DrawerLayout object */
 //                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
                 R.string.drawer_open,  /* "open drawer" description */
                 R.string.drawer_close  /* "close drawer" description */
         ) {
-
-            /**
-             * Called when a drawer has settled in a completely closed state.
-             */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                String mTitle = "Title";
-                getSupportActionBar().setTitle(mTitle);
-            }
-
-            /**
-             * Called when a drawer has settled in a completely open state.
-             */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                String mDrawerTitle = "MyDrawer";
-                getSupportActionBar().setTitle(mDrawerTitle);
-            }
         };
-// Set the drawer toggle as the DrawerListener
 
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("key",adapter.getItem(position).toString());
+                editor.commit();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        prefs = this.getPreferences(MODE_PRIVATE);
+
+
+        Button buttonSearch = (Button) findViewById(R.id.button_search);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+
+                                            @Override
+                                            public void onClick(View v) {
+
+                                            }
+                                        }
+        );
+
+        Button buttonHistory = (Button) findViewById(R.id.button_history);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+
+
+                                            }
+                                        }
+        );
+
 
         final ListView listView = (ListView) findViewById(R.id.list);
         imageAdapter = new ImageAdapter(this);
         listView.setAdapter(imageAdapter);
 
         final EditText editText = (EditText) findViewById(R.id.text_field);
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Photo photo = imageAdapter.getItem(position);
-                String title = photo.getTitle();
-                String url = photo.getUrl();
-                Intent intent = new Intent(ImageActivity.this, PhotoVerticale.class);
-                intent.putExtra("Title", title);
-                intent.putExtra("URL", url);
-                startActivity(intent);
-            }
-        });
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                Photo photo = imageAdapter.getItem(position);
+                                                String title = photo.getTitle();
+                                                String url = photo.getUrl();
+                                                Intent intent = new Intent(ImageActivity.this, PhotoVerticale.class);
+                                                intent.putExtra("Title", title);
+                                                intent.putExtra("URL", url);
+                                                startActivity(intent);
+                                            }
+                                        }
+
+        );
 
         Button button = (Button) findViewById(R.id.button_ok);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!editText.getText().toString().equals("")) {
-                    Toast.makeText(ImageActivity.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
-                }
-                listOne.clear();
-                list.clear();
-                boundService.getPhoto(editText.getText().toString());
-                final String photoSearch = editText.getText().toString();
+        button.setOnClickListener(new View.OnClickListener()
 
-                for (int i = 0; i < list.size(); i++) {
-                    Photo photo = list.get(i);
-                    String title = list.get(i).getTitle();
-                    if (title.equals(photoSearch)) {
-                        listOne.add(new Photo(photo.getTitle(), photo.getUrl()));
-                        imageAdapter.setList(listOne);
+                                  {
+                                      @Override
+                                      public void onClick(View v) {
+                                          if (!editText.getText().toString().equals("")) {
+                                              Toast.makeText(ImageActivity.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
+                                          }
+                                          listOne.clear();
+                                          list.clear();
+                                          boundService.getPhoto(editText.getText().toString());
+                                          final String photoSearch = editText.getText().toString();
 
-                        break;
-                    } else {
-                        imageAdapter.setList(list);
-                    }
-                }
-                imageAdapter.notifyDataSetChanged();
-            }
-        });
+                                          for (int i = 0; i < list.size(); i++) {
+                                              Photo photo = list.get(i);
+                                              String title = list.get(i).getTitle();
+                                              if (title.equals(photoSearch)) {
+                                                  listOne.add(new Photo(photo.getTitle(), photo.getUrl()));
+                                                  imageAdapter.setList(listOne);
+
+                                                  break;
+                                              } else {
+                                                  imageAdapter.setList(list);
+                                              }
+                                          }
+                                          imageAdapter.notifyDataSetChanged();
+                                      }
+                                  }
+
+        );
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-
 
     @Override
     protected void onStart() {
-        super.onStart();
+        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
         Intent intent = new Intent(this, BoundService.class);
         bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
     }
 
     @Override
     protected void onStop() {
-        super.onStop();
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
         if (bound) {
             unbindService(mConnection);
             bound = false;
         }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
     }
 
     private ServiceConnection mConnection = new ServiceConnection() {
@@ -154,6 +221,59 @@ public class ImageActivity extends AppCompatActivity implements InterfaceRespons
         imageAdapter.setList(photos);
         imageAdapter.notifyDataSetChanged();
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Image Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+
+//    public void onButtonSearch() {
+//
+//    }
+//
+//
+//    public void onButtonHistory() {
+//
+//    }
+
 }
 
 
